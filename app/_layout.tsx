40c0762684c6a3de9ff938eeeb39570b35884049
Geoffrey Brunet <geoffrey.brunet@icloud.com>
2026-01-21 09:58:56 +0100

    clean project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,48 +1,5 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-import { Fonts } from '@/constants/theme';
-
-// Prevent the splash screen from auto-hiding before fonts are loaded
-SplashScreen.preventAutoHideAsync();
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
-  const [fontsLoaded] = useFonts({
-    [Fonts.bakbak]: require('../assets/fonts/BakbakOne-Regular.ttf'),
-    [Fonts.roboto]: require('../assets/fonts/Roboto-Regular.ttf'),
-    [Fonts.robotoBold]: require('../assets/fonts/Roboto-Bold.ttf'),
-    [Fonts.robotoMedium]: require('../assets/fonts/Roboto-Medium.ttf'),
-  });
-
-  useEffect(() => {
-    if (fontsLoaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
-
-  if (!fontsLoaded) {
-    return null;
-  }
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+  return <Stack />;
 }
